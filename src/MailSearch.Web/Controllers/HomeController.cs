fa3338b75ac3a2d28a87936dabca8633c8ef0cbb
John Thuneby <thuneby@yahoo.dk>
2026-04-05 18:54:29 +0200

    Add Web Project

---
+++ src/MailSearch.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using MailSearch.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MailSearch.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
